910ca87f8e6b1c9a67093e9128f253baae796316
prasadhase31 <prasadhase9325@gmail.com>
2026-03-26 23:17:18 +0530

    student table

---
+++ demo.sql
@@ -0,0 +1,15 @@
+CREATE TABLE student(
+id INT auto_increment PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+email VARCHAR(100) UNIQUE NOT NULL,
+gender ENUM('Male','Female','Other'),
+date_of_birth DATE,
+created_at timestamp default current_timestamp
+);
+
+insert into student values("Siddhi Hase","siddhi@gmail.com","Female","2003-06-10");
+USE demo;
+
+INSERT INTO student(name, email, gender, date_of_birth)
+VALUES ('Prasad Hase', 'siddhuu@gmail.com', 'Female', '2003-06-10');
+use demo;
