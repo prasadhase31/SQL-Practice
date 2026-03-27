c369946d20ed7f65e2c6f6233d8fa1dc4c6f7a7e
prasadhase31 <prasadhase9325@gmail.com>
2026-03-27 15:45:43 +0530

    employee table

--- demo.sql
+++ demo.sql
@@ -11,5 +11,5 @@ insert into student values("Siddhi Hase","siddhi@gmail.com","Female","2003-06-10
 USE demo;
 
 INSERT INTO student(name, email, gender, date_of_birth)
-VALUES ('Prasad Hase', 'siddhuu@gmail.com', 'Female', '2003-06-10');
+VALUES ('Prasad Hase', 'siddhu@gmail.com', 'Male', '2003-03-31');
 use demo;
