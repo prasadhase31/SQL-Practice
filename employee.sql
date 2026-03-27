29dcf1fa69950e7f4b383a82d659bf68e303219d
prasadhase31 <prasadhase9325@gmail.com>
2026-03-27 15:50:34 +0530

    employee table

---
+++ employee.sql
@@ -0,0 +1,17 @@
+create database Prasad;
+show databases;
+use Prasad;
+
+create table employee(id int auto_increment primary key,name varchar(20),salary double,address varchar(20),mobNo int);
+
+insert into employee(id,name,salary,address,mobNo) values (11,'Prash',45000,'Pune',9345281038);
+
+alter table employee modify mobNo varchar(10);
+
+insert into employee(id,name,salary,address,mobNo) values (11,'Prash',45000,'Pune',9345281038);
+
+update employee set salary=35000 where id=11;
+
+select * from employee;
+
+select name from employee;
