6d784cb300c5a0b5bcafbd643f91ba9b27f7041e
prasadhase31 <prasadhase9325@gmail.com>
2026-03-28 12:38:34 +0530

    employee table

--- employee.sql
+++ employee.sql
@@ -2,15 +2,19 @@ create database Prasad;
 show databases;
 use Prasad;
 
-create table employee(id int auto_increment primary key,name varchar(20),salary double,address varchar(20),mobNo int);
+create table employee(id int auto_increment primary key,name varchar(20),salary double,address varchar(20),mobNo int,date_of_birth date);
 
 insert into employee(id,name,salary,address,mobNo) values (11,'Prash',45000,'Pune',9345281038);
 
 alter table employee modify mobNo varchar(10);
 
-insert into employee(id,name,salary,address,mobNo) values (11,'Prash',45000,'Pune',9345281038);
+insert into employee(id,name,salary,address,mobNo,date_of_birth) values (11,'Prash',45000,'Pune','9345281038','2003-03-31');
 
 update employee set salary=35000 where id=11;
+
+select * from employee;
+
+insert into employee(id,name,salary,address,mobNo,date_of_birth) values (11,'Prash',45000,'Pune','93452838','2003-03-31');
 
 select * from employee;
 
@@ -18,13 +22,13 @@ select name from employee;
 
 delete from employee where id=11;
 
-create table emp(id int primary key,name varchar(20),salary double,address varchar(20),mobNo text(10));
+create table emp(id int primary key,name varchar(20),salary double,address varchar(20),mobNo text);
 
 insert into emp(id,name,salary,address,mobNo) values (1,'Siddhi',50000,'Pune',7372929172);
 select * from emp;
 select * from employee;
 
-insert into employee(id,name,salary,address,mobNo) values (2,'Prasad',45000,'Pune',8574635213);
+insert into employee(id,name,salary,address,mobNo,date_of_birth) values (2,'Prasad',45000,'Pune','8574635213','2003-03-31');
 select * from employee;
 
 select * from emp where name='Siddhi';
@@ -32,3 +36,5 @@ select * from emp where name='Siddhi';
 insert into emp(id,name,salary,address,mobNo) values (2,'Siddhi',50000,'Pune',7372929172),(3,'Siddhuu',50000,'Pune',7372929172);
 
 select * from emp;
+
+
