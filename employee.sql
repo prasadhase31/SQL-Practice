7aef7c7f0b5fb30fe43b0f855ed89d605f6c7bb0
prasadhase31 <prasadhase9325@gmail.com>
2026-03-28 10:36:59 +0530

    employee table

--- employee.sql
+++ employee.sql
@@ -26,3 +26,9 @@ select * from employee;
 
 insert into employee(id,name,salary,address,mobNo) values (2,'Prasad',45000,'Pune',8574635213);
 select * from employee;
+
+select * from emp where name='Siddhi';
+
+insert into emp(id,name,salary,address,mobNo) values (2,'Siddhi',50000,'Pune',7372929172),(3,'Siddhuu',50000,'Pune',7372929172);
+
+select * from emp;
