362791eeefd55c865b1654a265f088e8b01f8a34
prasadhase31 <prasadhase9325@gmail.com>
2026-04-21 17:53:19 +0530

    SQL

---
+++ practice.sql
@@ -0,0 +1,7 @@
+
+create database practice;
+use practice;
+
+create table emp(id int,name varchar(10),address varchar(10),mobNo varchar(10),date_of_birth date,salary double);
+
+insert into emp(id,name,address,mobNo,date_of_birth,salary) values(1,Prasad,'Pune',9325648321,'2003-03-31',45000);
