b51c254bfc62c2f23ed3ee8403b557b395cf79db
prasadhase31 <prasadhase9325@gmail.com>
2026-04-21 18:25:55 +0530

    SQL

--- practice.sql
+++ practice.sql
@@ -1,7 +1,18 @@
 
-create database practice;
-use practice;
+-- create database practice1;
+-- use practice1;
+-- show databases;
+
+-- create table emp(id int,name varchar(10),address varchar(10),mobNo varchar(10),date_of_birth date,salary double);
+
+-- insert into emp(id,name,address,mobNo,date_of_birth,salary) values(1,'Prasad','Pune',9325648321,'2003-03-31',45000);
+
+create database clause;
+
+use clause;
 
 create table emp(id int,name varchar(10),address varchar(10),mobNo varchar(10),date_of_birth date,salary double);
 
-insert into emp(id,name,address,mobNo,date_of_birth,salary) values(1,Prasad,'Pune',9325648321,'2003-03-31',45000);
+ select * from emp;
+ 
+ 
