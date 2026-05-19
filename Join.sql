d51e804599572fb3023eecb9a3a50f7ce5468f7f
prasadhase31 <prasadhase9325@gmail.com>
2026-05-19 13:28:04 +0530

    SQL

---
+++ Join.sql
@@ -0,0 +1,41 @@
+create database joins;
+use joins;
+
+create table employee(id int,name varchar(10),address varchar(10),mobNo text(10),salary double,dept_id int);
+
+insert into employee(id ,name ,address ,mobNo ,salary ,dept_id ) values(10,'Prasad','Pune','9325050515',45000,101);
+
+select * from employee;
+insert into employee(id ,name ,address ,mobNo ,salary ,dept_id ) values(11,'Siddhu','Mumbai','9126050515',50000,102);
+
+select * from employee;
+
+create table department(dept_id int,dept_name varchar(10));
+
+insert into department(dept_id,dept_name) values(101,'IT');
+insert into department(dept_id,dept_name) values(102,'HR');
+
+select * from department;
+drop table department;
+
+select e.name,d.dept_name from employee e inner join department d on e.dept_id=d.dept_id;
+
+select e.name,d.dept_name from employee e left join department d on e.dept_id=d.dept_id;
+
+select e.name,d.dept_name from employee e right join department d on e.dept_id=d.dept_id;
+
+select e.name,d.dept_name from employee e cross join department d;
+
+select e.name,d.dept_name
+from employee e
+left join department d
+on e.dept_id=d.dept_id
+
+union
+
+select e.name,d.dept_name
+from employee e
+right join department d
+on e.dept_id=d.dept_id;
+
+ 
