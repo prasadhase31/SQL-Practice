f0d048bbb78622397135e7d73f2abac6335a907e
prasadhase31 <prasadhase9325@gmail.com>
2026-05-25 11:36:50 +0530

    SQL

---
+++ jdbc.sql
@@ -0,0 +1,9 @@
+create database JDBC;
+use JDBC;
+show databases;
+
+create table Students(id int auto_increment primary key,name varchar(255) not null,age int not null,marks double not null);
+describe Students;
+insert into Students(name,age,marks)values("Prasad",23,95),("Siddhu",22,94);
+select * from Students;
+delete from Students where id=1;
