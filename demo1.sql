d54bfd6693e50958f828642e60a809346cfebd66
prasadhase31 <prasadhase9325@gmail.com>
2026-05-26 13:49:10 +0530

    SQL

--- demo1.sql
+++ demo1.sql
@@ -5,3 +5,4 @@ create table student(id int,name varchar(30),marks double,age int);
 insert into student(id,name,marks,age) values(1,"Prasad",91,23),(2,"Siddhu",93,22);
 
 select * from student;
+select name from student where id=1;
