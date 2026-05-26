cace7a26019cbe406a40c25cc15ed0fad4718599
prasadhase31 <prasadhase9325@gmail.com>
2026-05-26 13:24:07 +0530

    SQL

--- demo1.sql
+++ demo1.sql
@@ -3,3 +3,5 @@ use mcs;
 
 create table student(id int,name varchar(30),marks double,age int);
 insert into student(id,name,marks,age) values(1,"Prasad",91,23),(2,"Siddhu",93,22);
+
+select * from student;
