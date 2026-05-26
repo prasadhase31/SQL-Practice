bf320abd8d32a15313947f8e5fc7b0ccfcf97a3c
prasadhase31 <prasadhase9325@gmail.com>
2026-05-26 13:17:23 +0530

    SQL

--- demo1.sql
+++ demo1.sql
@@ -2,3 +2,4 @@ create database mcs;
 use mcs;
 
 create table student(id int,name varchar(30),marks double,age int);
+insert into student(id,name,marks,age) values(1,"Prasad",91,23),(2,"Siddhu",93,22);
