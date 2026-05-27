da699d9aa8efe17d54dc47c6748367865bc74cf3
prasadhase31 <prasadhase9325@gmail.com>
2026-05-27 10:56:25 +0530

    SQL

--- CRUD.sql
+++ CRUD.sql
@@ -2,3 +2,4 @@ create database crud;
 use crud;
 create table Student(id int primary key,name varchar(10),age int,marks double);
 insert into Student(id,name,age,marks) values(1,"Siddhu",23,93),(2,"Prasad",24,90);
+select * from Student;
